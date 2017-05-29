ddc5cdeb799df081ce54a52af54ec16f349f60cb
Boucard Delphine <delphine96.boucard@orange.fr>
2017-05-29 22:39:49 +0200

    Trebuchet affichage complet sans terrain

--- glwindow.h
+++ glwindow.h
@@ -2,6 +2,7 @@
 #define GLWINDOW_H
 
 #include "glwidget.h"
+#include <QOpenGLTexture>
 
 class GLWindow : public GLWidget
 {
@@ -10,15 +11,24 @@ class GLWindow : public GLWidget
 private:
     int yRot;
     GLuint GLdisplayList[1];
+    QOpenGLTexture *texture;
 public:
+
     explicit GLWindow(QWidget *parent = 0);
     void initializeGL();
     void resizeGL(int width, int height);
     void paintGL();
+
     void drawTerrain();
-    void drawTrebuchet(int rotX, int rotY);
     void drawFence();
-    void displayCDVDisplayList();
+
+    void drawTrebuchet(int rotX, int rotY);    
+    void drawPlankClose();
+    void drawPlankOpen();
+    void drawCylinder();
+    void drawArmature();
+
+    void loadAndBlind(char* filename);
 
 public slots:
     void setYRotation(int angle);
